3377506d1e3e04c912466397b4b64b411b071f23
siavash <siamhb@protonmail.com>
2022-06-01 00:25:48 +0430

    i write the initial style of base the project and im gonna work on components

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+window.addEventListener("resize", () => {
+  document.getElementById("root").width = window.innerWidth;
+});
+
+
+
+function App() {
+  	<>
+  	<h1> HELLO DEAR</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import "./App/styles/index.css"
+import App from "./App/App"
+const root = createRoot(document.getElementById("root"))
+root.render(<App/>)
